fa32458f5ac61b22f48a561a5c1223d5ffbc92f4
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-04-16 21:42:06 +0330

    Nested Animations with Variants

--- React-Challenges/src/components/NewChallenge.jsx
+++ React-Challenges/src/components/NewChallenge.jsx
@@ -1,4 +1,5 @@
 import { useContext, useRef, useState } from 'react';
+import { motion } from 'framer-motion';
 
 import { ChallengesContext } from '../store/challenges-context.jsx';
 import images from '../assets/images.js';
@@ -59,13 +60,19 @@ export default function NewChallenge({ onDone }) {
 
         <ul id="new-challenge-images">
           {images.map((image) => (
-            <li
+            <motion.li
+              variants={{
+                hidden: { opacity: 0, scale: 0.3 },
+                visible: { opacity: 1, scale: 1 },
+              }}
+              exit={{ opacity: 1, scale: 1 }}
+              transition={{ type: 'spring' }}
               key={image.alt}
               onClick={() => handleSelectImage(image)}
               className={selectedImage === image ? 'selected' : undefined}
             >
               <img {...image} />
-            </li>
+            </motion.li>
           ))}
         </ul>
 
@@ -80,3 +87,15 @@ export default function NewChallenge({ onDone }) {
     </Modal>
   );
 }
+ 
+/* why we should not use:  exit="hidden"
+- if you use this the "entry animation" of these "images" will gone
+
+- if you set one of those animation-props like initial, animate, exit on some child-component when using that "variants" feature to activate animations
+
+- you must not use one of your variant names here in child-component
+
+instead you should use it like this:   exit={{ opacity: 1, scale: 1 }}
+
+- to not causing this delay in the removal here in "images"
+*/
