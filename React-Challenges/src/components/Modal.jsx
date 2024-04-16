6996bd5e879739eba781b73713e9365fd9e6f3c2
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-04-16 18:56:04 +0330

    Adding Entry Animations - initial

--- React-Challenges/src/components/Modal.jsx
+++ React-Challenges/src/components/Modal.jsx
@@ -1,16 +1,27 @@
 import { createPortal } from 'react-dom';
+import { motion } from 'framer-motion';
 
 export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
 
-      <dialog open className="modal">
+      <motion.dialog
+        initial={{ opacity: 0, y: -30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.4 }}
+        open
+        className="modal"
+      >
         <h2> {title} </h2>
 
         {children}
-      </dialog>
+      </motion.dialog>
     </>,
     document.getElementById('modal')
   );
 }
+
+/*
+initial: will be assumed immediately after this element has been added to the DOM - set the starting state
+*/
