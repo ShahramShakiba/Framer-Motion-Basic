74678097c8c6729c1af4ab58d01b5c359bbfc0a4
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-04-16 20:57:47 +0330

    Making Elements Pop With Hover Animations

--- React-Challenges/src/components/Header.jsx
+++ React-Challenges/src/components/Header.jsx
@@ -1,7 +1,7 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import { useState } from 'react';
 
 import NewChallenge from './NewChallenge.jsx';
-import { AnimatePresence } from 'framer-motion';
 
 export default function Header() {
   const [isCreatingNewChallenge, setIsCreatingNewChallenge] = useState();
@@ -23,14 +23,23 @@ export default function Header() {
       <header id="main-header">
         <h1> Your Challenges </h1>
 
-        <button onClick={addNewChallengeHandler} className="button">
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          transition={{ type: 'spring', stiffness: 300}}
+          onClick={addNewChallengeHandler}
+          className="button"
+        >
           Add Challenge
-        </button>
+        </motion.button>
       </header>
     </>
   );
 }
 
 /* AnimatePresence
-to be used as a wrapper around the code that conditionally displays or removes elements if you wanna animate those elements - especially to animate the "disappearance" of those elements
+- to be used as a wrapper around the code that conditionally displays or removes elements if you wanna animate those elements - especially to animate the "disappearance" of those elements
+*/
+
+/* AnimatePresence
+- 
 */
