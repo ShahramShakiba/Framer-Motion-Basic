60837d4e46f5219eb8447c6538a8a30b4f832422
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-04-17 12:20:21 +0330

    Using AnimatePresence with Multi-Elements to control Animations

--- React-Challenges/src/components/Challenges.jsx
+++ React-Challenges/src/components/Challenges.jsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import { useContext, useState } from 'react';
 
 import { ChallengesContext } from '../store/challenges-context.jsx';
@@ -42,21 +43,52 @@ export default function Challenges() {
         onSelectType={handleSelectType}
         selectedType={selectedType}
       >
-        {displayedChallenges.length > 0 && (
-          <ol className="challenge-items">
-            {displayedChallenges.map((challenge) => (
-              <ChallengeItem
-                key={challenge.id}
-                challenge={challenge}
-                onViewDetails={() => handleViewDetails(challenge.id)}
-                isExpanded={expanded === challenge.id}
-              />
-            ))}
-          </ol>
-        )}
+        {/* disappears items with help of "AnimatePresence" */}
+        <AnimatePresence mode="wait">
+          {displayedChallenges.length > 0 && (
+            <motion.ol
+              key="list"
+              exit={{ y: -40, opacity: 0, transition: { duration: 0.3 } }}
+              className="challenge-items"
+            >
+              <AnimatePresence>
+                {displayedChallenges.map((challenge) => (
+                  <ChallengeItem
+                    key={challenge.id}
+                    challenge={challenge}
+                    onViewDetails={() => handleViewDetails(challenge.id)}
+                    isExpanded={expanded === challenge.id}
+                  />
+                ))}
+              </AnimatePresence>
+            </motion.ol>
+          )}
 
-        {displayedChallenges.length === 0 && <p> No challenges found. </p>}
+          {displayedChallenges.length === 0 && (
+            <motion.p
+              key="fallback"
+              initial={{ opacity: 0, y: -25 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5 }}
+              exit={{ opacity: 0, y: -25 }}
+            >
+              No challenges found.
+            </motion.p>
+          )}
+        </AnimatePresence>
       </ChallengeTabs>
     </div>
   );
 }
+
+/* key
+- to get back the removal animation back we need to tell framer-motion to set apart "ol" and "p" from each other
+*/
+
+/* mode
+- default is mode='sync':
+  - sync: all children are removed at the same time
+
+- mode='wait':
+  - wait: children are removed one by one
+*/
